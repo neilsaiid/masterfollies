3f5674d5b87e8169a7add130d24801b999045fee
neilsaiid <neil.saiid@gmail.com>
2013-01-21 00:51:14 -0600

    the rest of the test files.

---
+++ src/test/java/edu/lcu/masterfollies/domain/QuestionsTest.java
@@ -0,0 +1,53 @@
+package edu.lcu.masterfollies.domain;
+import static org.junit.Assert.assertTrue;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+
+
+
+@Configurable
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {"classpath:/META-INF/spring/applicationContext.xml", 
+"classpath:/META-INF/spring/applicationContext-datasource-test.xml"})
+public class ClubNamesTest implements BeanFactoryAware {
+	
+	private static Log log = LogFactory.getLog(ClubNamesTest.class);
+	BeanFactory bf = null;
+    
+	//@Override
+	public void setBeanFactory(BeanFactory beanFactory) throws BeansException {
+		bf = beanFactory;
+	}
+	@Test
+	public void testselectByPrimaryKey(){
+		ClubNamesMapper c = (ClubNamesMapper) bf.getBean(ClubNamesMapper.class);
+		ClubNames ret = c.selectByPrimaryKey(1);
+		System.out.println("Club Name for id " + ret.getId() + "=" + ret.getClubName());
+	}
+	//will always pass.
+	@Test
+	public void testClubNameAddInsertandRemove(){
+		ClubNamesMapper c = (ClubNamesMapper) bf.getBean(ClubNamesMapper.class);		//Judges judges = new Judges("Neil","Saiid","nsaiid","test");
+		ClubNames cn = new ClubNames();
+		cn.setClubName("CD's");
+		System.out.println("Club Name to be added= " + cn.getClubName());
+		c.insertSelective(cn);
+		System.out.println("Club Name that was inserted= " + cn.getClubName());
+		ClubNames p= c.selectByPrimaryKey(cn.getId());
+		System.out.println(p.getClubName());
+		//assertTrue(p.equals(judge));
+		c.deleteByPrimaryKey(cn.getId());
+		System.out.println("Club: " + cn.getClubName() +	" has been deleted successfully.");
+	}
+}
+	
