055a7318ce4194517c30ddd5270d18d950e1aa9f
Neil Saiid <neil.saiid@gmail.com>
2013-01-30 15:17:49 -0600

    tracking a new results View impl

---
+++ src/main/java/edu/lcu/masterfollies/client/ui/ResultsViewImpl.java
@@ -0,0 +1,50 @@
+package edu.lcu.masterfollies.client.ui;
+
+import java.util.Map;
+
+import com.google.gwt.user.cellview.client.CellTable;
+import com.google.gwt.user.client.ui.Button;
+import com.google.gwt.user.client.ui.Composite;
+import com.google.gwt.user.client.ui.TabBar;
+
+import edu.lcu.masterfollies.domain.Results;
+
+public class ResultsViewImpl extends Composite implements ResultsView {
+	public ResultsViewImpl() {
+	}
+
+	@Override
+	public void setLblTitle(String string) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+
+	@Override
+	public void setListener(Presenter listener) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void setTab() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public TabBar getTabBar() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public CellTable<Results> getTblResultsList() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+
+}
